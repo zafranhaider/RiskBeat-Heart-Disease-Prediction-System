e5d19af783035d0c8afce55431f899733c427e04
zafranhaider <zraaeae@gmail.com>
2025-03-30 04:58:43 +0500

    health_mod_complete

--- health_deals/urls.py
+++ health_deals/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, upload_notification
+from .views import home, upload_notification,notification_detail
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', home, name='home3'),
     path('upload1/', upload_notification, name='upload_notification'),
+    path('notification/<int:pk>/', notification_detail, name='notification_detail'),
 ]
--- health_deals/views.py
+++ health_deals/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Notification
-
+from django.shortcuts import render, get_object_or_404
 # Home View - Displays notifications
 def home(request):
       notifications = Notification.objects.all().order_by('-id')  # Fetch latest notifications
       return render(request, 'home1.html', {'notifications': notifications})
+  
+def notification_detail(request, pk):
+    notification = get_object_or_404(Notification, pk=pk)
+    return render(request, 'view.html', {'notification': notification})
 
 # Upload Notification View - Handles manual form submissions
 def upload_notification(request):
