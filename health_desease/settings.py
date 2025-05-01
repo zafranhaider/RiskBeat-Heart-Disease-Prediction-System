e1c3216dabc8fbe196a75cf97cf53e399a0007d8
Syed Zafran Haider kazmi <113259072+zafranhaider@users.noreply.github.com>
2025-05-01 08:23:52 +0500

    Update settings.py

--- health_desease/settings.py
+++ health_desease/settings.py
@@ -130,12 +130,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = Path.joinpath(BASE_DIR,'media')
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 #Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
